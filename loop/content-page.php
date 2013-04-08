27a071d627721c31b47c7a552c0bbd538acf23f0
longster <longster@live.com>
2013-04-08 09:43:31 -0500

    update gitignore

--- loop/content-page.php
+++
@@ -1,9 +0,0 @@
-
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-		<div class="entry-content">
-			<?php the_content(); ?>
-			<?php wp_link_pages( array( 'before' => '<div class="page-links">' . __( 'Pages:', 'wpzurb' ), 'after' => '</div>' ) ); ?>
-		</div><!-- .entry-content -->
-
-	</article><!-- #post -->
--- sidebar.php
+++
@@ -1,7 +0,0 @@
-<div class="large-3 columns <?php if (is_page_template('templates/_content_left.php')) : ?><?php else : ?>pull-9<?php endif; ?>">
-	<div class="panel">
-	<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Sidebar')) : ?>
-		Configure at Dashboard > Appearance > Widget > Sidebar
-	<?php endif; ?>
-	</div>
-</div>
