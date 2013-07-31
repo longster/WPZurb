07ddc61dae413a6a1fca55ca8bf49add110f3010
longster <longster@live.com>
2013-07-31 11:29:28 -0500

    Update js to 4.3

--- footer.php
+++ footer.php
@@ -46,18 +46,20 @@
 	</script>
 
 	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.alerts.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.clearing.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.cookie.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.dropdown.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.forms.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.joyride.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.magellan.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.orbit.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.placeholder.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.reveal.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.section.js"></script>
-	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.tooltips.js"></script>
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.abide.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.alerts.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.clearing.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.cookie.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.dropdown.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.forms.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.interchange.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.joyride.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.magellan.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.orbit.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.placeholder.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.reveal.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.section.js"></script>	
+	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.tooltips.js"></script>	
 	<script src="<?php bloginfo( 'template_url' );?>/js/foundation/foundation.topbar.js"></script>
 	
 	<script>
